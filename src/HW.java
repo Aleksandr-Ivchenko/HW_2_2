fd3be78533e0b21e72a65430e72f2e41385997e8
Aleksandr-Ivchenko <85690047+Aleksandr-Ivchenko@users.noreply.github.com>
2021-07-14 22:58:49 +0300

    Update HW.java

--- src/HW.java
+++ src/HW.java
@@ -1,32 +1,30 @@
-.
-public  class  HW {
-    public  static  void  main ( String [] args ) {
-        Строка arr [] [] = {{ " 1 " , " 2 " , " 3 " , " f " }, { " 5 " , " 6 " , " r " , " 8 " }};
-        int sum =  0 ;
-        пытаться
+public class HW {
+    public static void main (String[] args) {
+        String arr[][] = {{"1","2","3","f" },{"5","6","r","8"}};
+        int sum = 0;
+        try
         {
-            Система . из . println (arr [ 1 ] [ 4 ]);
+            System.out.println(arr[1][4]);
         }
-        catch ( ArrayIndexOutOfBoundsException e1)
+        catch (ArrayIndexOutOfBoundsException e1)
         {
-            Система . из . println ( " MyArraySizeException: "  + e1 . getMessage ());
+            System.out.println("MyArraySizeException :" + e1.getMessage());
         }
-         for ( int i =  0 ; i <  2 ; i ++ ) {
-                для ( int j =  0 ; j <  4 ; j ++ )
+         for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 4; j++)
                 {
-                    пытаться
+                    try
                     {
-                   int c =  целое число . parseInt (arr [i] [j] . trim ());
-                   сумма = сумма + с;
-                    Система . из . println (c);
+                   int c = Integer.parseInt(arr[i][j].trim());
+                   sum = sum +c;
+                    System.out.println(c);
                     }
-                поймать ( NumberFormatException nfe)
+                catch (NumberFormatException nfe)
                 {
-                    Система . из . println ( " MyArrayDataException: "  + нф . getMessage ());
+                    System.out.println("MyArrayDataException :" + nfe.getMessage());
                 }
                 }
-             Система . из . println ( " Сумма валидных ячеек: "  + сумма);
+             System.out.println("Сумма валидных ячеек: " + sum);
                 }
     }
 }
-
