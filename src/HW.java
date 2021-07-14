9029e5dfba78ba9253158b15486ea7c14caf08ff
Aleksandr-Ivchenko <85690047+Aleksandr-Ivchenko@users.noreply.github.com>
2021-07-14 22:54:19 +0300

    Update HW.java

--- src/HW.java
+++ src/HW.java
@@ -1,34 +1,32 @@
-public class HW {
-    public static void main (String[] args) {
-        String arr[][] = {{"1","2","3","f" },{"5","6","r","8"}};
-        int sum = 0;
-        try
+.
+public  class  HW {
+    public  static  void  main ( String [] args ) {
+        Строка arr [] [] = {{ " 1 " , " 2 " , " 3 " , " f " }, { " 5 " , " 6 " , " r " , " 8 " }};
+        int sum =  0 ;
+        пытаться
         {
-            System.out.println(arr[1][4]);
+            Система . из . println (arr [ 1 ] [ 4 ]);
         }
-        catch (ArrayIndexOutOfBoundsException e1)
+        catch ( ArrayIndexOutOfBoundsException e1)
         {
-            System.out.println("MyArraySizeException :" + e1.getMessage());
+            Система . из . println ( " MyArraySizeException: "  + e1 . getMessage ());
         }
-         for (int i = 0; i < 2; i++) {
-                for (int j = 0; j < 4; j++)
+         for ( int i =  0 ; i <  2 ; i ++ ) {
+                для ( int j =  0 ; j <  4 ; j ++ )
                 {
-                    try
+                    пытаться
                     {
-                   int c = Integer.parseInt(arr[i][j].trim());
-                   sum = sum +c;
-                    System.out.println(c);
+                   int c =  целое число . parseInt (arr [i] [j] . trim ());
+                   сумма = сумма + с;
+                    Система . из . println (c);
                     }
-                catch (NumberFormatException nfe)
+                поймать ( NumberFormatException nfe)
                 {
-                    System.out.println("MyArrayDataException :" + nfe.getMessage());
+                    Система . из . println ( " MyArrayDataException: "  + нф . getMessage ());
                 }
                 }
-             System.out.println("Сумма валидных ячеек: " + sum);
+             Система . из . println ( " Сумма валидных ячеек: "  + сумма);
                 }
     }
 }
-
-
-
 
